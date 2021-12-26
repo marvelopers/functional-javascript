d53c00987473e5f53aecf6de533af369e60c6650
marvelopers <marvelopers@gmail.com>
2021-12-26 23:27:29 +0900

    map, filter, reduce, curry 구현

---
+++ fs/lib.js
@@ -0,0 +1,33 @@
+const map = (f, iter) => {
+  let res = [];
+  for(const a of iter){
+    res.push(f(a));
+  }
+
+  return res
+}
+
+const filter = (f, iter) => {
+  let res = [];
+  for(const a of iter){
+    if(f(a)) res.push(f(a));
+  }
+
+  return res
+}
+
+
+const reduce = (f, acc, iter) => {
+  if(!iter){
+    iter = acc[Symbol.iterator()];
+    acc = iter.next().value;
+  }
+  for(const a of iter){
+    acc = f(acc, a)
+  }
+
+  return acc
+}
+
+const curry = f => 
+  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
